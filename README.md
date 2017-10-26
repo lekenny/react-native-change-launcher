1e21090034b879c77585eb6f7d6d1ebcbb8e5dc7
lekenny <lekenny.cn@gmail.com>
2017-10-26 17:02:55 +0800

    init android example

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # react-native-change-launcher
-动态更换appIcon
+Dynamic replace launcher icon
+## Installation
+
+Install package from npm
+
+```sh
+yarn add --save react-native-change-launcher
+```
+
+## Usage
+
+ES6
+
+The module uses ES6 style export statement, simply use `import` to load the module.
+
+```js
+import changeLuancher from 'react-native-change-launcher'
+```
+
+ES5
+
+If you're using ES5 require statement to load the module, please add `default`. See [here](https://github.com/wkh237/react-native-fetch-blob/wiki/Trouble-Shooting#rnfetchblobfetch-is-not-a-function) for more detail.
+
+```
+var changeLuancher = require('react-native-change-launcher').default
+```
---
+++ examples/App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+import changeLuancher from 'react-native-change-launcher'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+          To get started, edit App.js
+        </Text>
+        <TouchableOpacity onPress={()=>{
+          console.log(changeLuancher);
+          changeLuancher('com.awesomeproject.MainActivity2');
+        }}>
+          <Text style={styles.welcome}>
+            点击更换Icon
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
